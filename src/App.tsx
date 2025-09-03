8add77b8b4a63b55f877d7f38765a0000d5e127f
nithin <nithincr71609@gmail.com>
2025-09-03 12:24:17 +0530

    fd

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <Routes>
           <Route path="/" element={<SplashScreen />} />
           <Route path="/language" element={<LanguageSelection />} />
